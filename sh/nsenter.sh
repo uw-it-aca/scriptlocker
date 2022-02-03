176e37f9ad4495f909b72cc7e9b4961b0e4fd633
Paul Marcontell <paulkm@uw.edu>
2022-02-03 10:09:04 -0800

    added nsenter script

---
+++ sh/nsenter.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# https://github.com/alexei-led/nsenter
+# Create privileged pod and uses the nsenter command to start a shell on the host as root
+# https://man7.org/linux/man-pages/man1/nsenter.1.html
+
+if [ $# == 0 ] || [ $1 == "-h" ] || [ $1 == "--help" ]; then
+    echo "Usage: $0 k8s_node_name"
+    exit 1
+fi
+
+set -x
+
+node=${1}
+nodeName=$(kubectl get node ${node} -o template --template='{{index .metadata.labels "kubernetes.io/hostname"}}')
+nodeSelector='"nodeSelector": { "kubernetes.io/hostname": "'${nodeName:?}'" },'
+podName=${USER}-nsenter-${node}
+# convert @ to -
+podName=${podName//@/-}
+# convert . to -
+podName=${podName//./-}
+# truncate podName to 63 characters which is the kubernetes max length for it
+podName=${podName:0:63}
+
+kubectl run ${podName:?} --restart=Never -it --rm --image overriden --overrides '
+{
+  "spec": {
+    "hostPID": true,
+    "hostNetwork": true,
+    '"${nodeSelector?}"'
+    "tolerations": [{
+        "operator": "Exists"
+    }],
+    "containers": [
+      {
+        "name": "nsenter",
+        "image": "alexeiled/nsenter",
+        "command": [
+          "/nsenter", "--all", "--target=1", "--", "su", "-"
+        ],
+        "stdin": true,
+        "tty": true,
+        "securityContext": {
+          "privileged": true
+        },
+        "resources": {
+          "requests": {
+            "cpu": "10m"
+          }
+        }
+      }
+    ]
+  }
+}' --attach "$@"

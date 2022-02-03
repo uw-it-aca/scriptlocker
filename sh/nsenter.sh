fe591dc7df1e6288a5a5c27e1241e6edb8c41a8d
Paul Marcontell <paulkm@uw.edu>
2022-02-03 10:26:32 -0800

    add help text, disable debug output

--- sh/nsenter.sh
+++ sh/nsenter.sh
@@ -2,14 +2,15 @@
 
 # https://github.com/alexei-led/nsenter
 # Create privileged pod and uses the nsenter command to start a shell on the host as root
-# https://man7.org/linux/man-pages/man1/nsenter.1.html
+# automatically tears itself down on exit
 
 if [ $# == 0 ] || [ $1 == "-h" ] || [ $1 == "--help" ]; then
+    echo "Get a shell with root privileges on specified Kubernetes node"
     echo "Usage: $0 k8s_node_name"
     exit 1
 fi
 
-set -x
+# set -x
 
 node=${1}
 nodeName=$(kubectl get node ${node} -o template --template='{{index .metadata.labels "kubernetes.io/hostname"}}')
@@ -21,6 +22,8 @@ podName=${podName//@/-}
 podName=${podName//./-}
 # truncate podName to 63 characters which is the kubernetes max length for it
 podName=${podName:0:63}
+
+echo "With great power comes great responsibility"
 
 kubectl run ${podName:?} --restart=Never -it --rm --image overriden --overrides '
 {
